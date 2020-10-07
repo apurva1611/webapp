20ba7634509d30c9b40f9fa824bb6c8708cbadb2
Boran Yildirim <boranyldrm21@gmail.com>
2020-10-07 10:53:15 +0300

    password will not return as a response, used omitempty

--- webapp/user.go
+++ webapp/user.go
@@ -4,7 +4,7 @@ type User struct {
 	ID             string `json:"id"`
 	FirstName      string `json:"first_name" binding:"required"`
 	LastName       string `json:"last_name" binding:"required"`
-	Password       string `json:"password" binding:"required"`
+	Password       string `json:"password,omitempty" binding:"required"`
 	Username       string `json:"username" binding:"required"`
 	AccountCreated string `json:"account_created"`
 	AccountUpdated string `json:"account_updated"`
