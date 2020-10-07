94996b0d892d4f47f56102aac5db4faccc9adb84
Boran Yildirim <boranyldrm21@gmail.com>
2020-10-07 15:16:17 +0300

    main test

--- webapp/main_test.go
+++ webapp/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,14 +19,14 @@ func TestCreateUserSuccess(t *testing.T) {
 	req, _ := http.NewRequest("POST", "/v1/user", bytes.NewBuffer(jsonStr))
 	router.ServeHTTP(w, req)
 
-	var response map[string]string
+	log.Print(w.Body.String())
+	var response map[string]interface{}
 	err := json.Unmarshal([]byte(w.Body.String()), &response)
+	assert.Nil(t, err)
 
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Nil(t, err)
-	assert.Equal(t, response["first_name"], "Jane")
-	assert.Equal(t, response["last_name"], "Doe")
-	assert.Equal(t, response["username"], "jane.doe@example.com")
+	assert.Contains(t, response, "token")
+	assert.Contains(t, response, "user")
 }
 
 func TestCreateUserFail(t *testing.T) {
@@ -37,4 +38,41 @@ func TestCreateUserFail(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestGetUserSelf(t *testing.T) {
+	router := SetupRouter()
+
+	// create user to get the token
+	w := httptest.NewRecorder()
+	var jsonStr = []byte("{\n  \"first_name\": \"Jane\",\n  \"last_name\": \"Doe\",\n  \"password\": \"skdjfhskdfjhg\",\n  \"username\": \"jane.doe@example.com\"\n}")
+	req, _ := http.NewRequest("POST", "/v1/user", bytes.NewBuffer(jsonStr))
+	router.ServeHTTP(w, req)
+
+	log.Print(w.Body.String())
+	var response map[string]interface{}
+	err := json.Unmarshal([]byte(w.Body.String()), &response)
+	assert.Nil(t, err)
+
+	token := response["token"]
+
+	// try to get self
+	w = httptest.NewRecorder()
+	var bearer string
+	if val, ok := token.(string); ok {
+		bearer = "Bearer " + val
+	}
+	log.Print(bearer)
+	req, _ = http.NewRequest("GET", "/v1/user/self", nil)
+	req.Header.Set("Authorization", bearer)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, w.Code, http.StatusOK)
+
+	log.Print(w.Body.String())
+	var response2 map[string]interface{}
+	err = json.Unmarshal([]byte(w.Body.String()), &response2)
+	assert.Nil(t, err)
+
+	assert.Contains(t, response2, "id")
 }
